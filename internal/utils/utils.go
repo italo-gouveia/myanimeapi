4b2a6965ef487ed00a58114d2b158fc5e547cb95
José Italo Gouveia Soares <italo_2512@hotmail.com>
2025-02-23 15:33:54 -0300

    [GITISSUE-9][GITISSUE-13] Adding documentation to the packages: utils, errors, database auth, adding logging to have informations across the usage of it, adjusted the errors to have a reusage of it

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -4,12 +4,18 @@ package utils
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
+	"log"
 )
 
 // GenerateRandomString generates a random string of the specified length.
 func GenerateRandomString(length int) (string, error) {
+	if length <= 0 {
+		return "", fmt.Errorf("length must be greater than 0")
+	}
 	bytes := make([]byte, length)
 	if _, err := rand.Read(bytes); err != nil {
+		log.Printf("Error generating random bytes: %v", err)
 		return "", err
 	}
 	return hex.EncodeToString(bytes), nil
